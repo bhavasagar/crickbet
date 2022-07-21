5f9eff0915a3354feb23940419e9b221afa85441
bhavasagar09 <bhavasagar.putta@quantrium.ai>
2022-07-21 11:28:43 +0530

    Mail change

--- server/crickbet/crickbet/settings/development.py
+++ server/crickbet/crickbet/settings/development.py
@@ -23,11 +23,11 @@ if config('POSTGRESS') == 'TRUE':
     }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'officialstartgain.gamil.com'
-EMAIL_HOST_PASSWORD = 'Hp@pavilion144hz'
+EMAIL_PORT = int(config('EMAIL_PORT'))
+EMAIL_HOST_USER = config('EMAIL_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_PASSWORD')
 
 
 UI_WEBSITE_URL = "http://localhost:3000/"
